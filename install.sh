2b0dfdb385e112538944266f127b9883245c5c4b
CS8ABG <cs8abg@gmail.com>
2024-05-28 12:29:27 +0000

    initial commit

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+show_adnsystems() {
 clear
 
 echo "                  _____  _   _    _____           _                     "
@@ -12,6 +13,16 @@ echo "                                         __/ |                          "
 echo "                                        |___/                           "
 echo "                                                                        "
 echo "                                                                        "
+}
+show_adnsystems
+echo "************************************************************************"
+echo "*                                                                      *"
+echo "* This script will install or upgrade ADN Systems Server and Dashboard *"
+echo "*                                                                      *"
+echo "************************************************************************"
+echo "                                                                        "
+echo "                                                                        "
+
 
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
@@ -26,83 +37,201 @@ if [[ "$ID" != "debian" && "$ID" != "ubuntu" ]]; then
   exit 1
 fi
 
-if [[ "$VERSION_ID" != "10" && "$VERSION_ID" != "11" && "$VERSION_ID" != "18.04" && "$VERSION_ID" != "20.04" && "$VERSION_ID" != "22.04" ]]; then
-  echo "Unsupported Debian/Ubuntu version. Supported versions: Debian 10, Debian 11, Ubuntu 18.04, 20.04, 22.04."
+if [[ "$VERSION_ID" != "11" && "$VERSION_ID" != "20.04" && "$VERSION_ID" != "22.04" ]]; then
+  echo "Unsupported Debian/Ubuntu version. Supported versions: Debian 11, Ubuntu 20.04, 22.04."
   exit 1
 fi
 
 # Install net-tools if not installed
 if ! command -v netstat &> /dev/null; then
   echo "Installing net-tools..."
-  apt update > /dev/null
-  apt install -y net-tools > /dev/null
+  apt-get update > /dev/null
+  apt-get install -y net-tools > /dev/null
 fi
 
-# Check if TCP port 9000 is in use and /opt/dashboard does not exist
-if netstat -tuln | grep ':9000' > /dev/null && [ ! -d /opt/dashboard ]; then
-  echo "It seems that you have a custom dashboard installation already installed. Please remove the current installation and run this script again."
-  exit 1
-fi
-
-# Check if the dashboard is already installed
-if [ -d /opt/dashboard ]; then
-  read -p "Dashboard is already installed. Do you want to update it from the latest version on GitHub? (y/n): " update_choice
-  if [[ "$update_choice" != "y" && "$update_choice" != "Y" ]]; then
-    echo "Exiting without making any changes."
-    exit 0
+# Function to install/update the ADN Server
+install_update_server() {
+  # Check if TCP port 4321 is in use and /opt/adnserver does not exist
+  if netstat -tuln | grep ':4321' > /dev/null && [ ! -d /opt/adnserver ]; then
+    echo "It seems that you have a custom ADN server installation already installed. Please remove the current installation and run this script again."
+    exit 1
   fi
-  echo "Stopping services before updating..."
-  systemctl stop adn_proxy.service > /dev/null
-  systemctl stop adn_dashboard.service > /dev/null
-  echo "Updating the dashboard..."
-else
-  echo "Installing the dashboard..."
 
-  # Update and install required packages
-  echo "Updating package list and installing required packages..."
-  apt update > /dev/null
-  apt install -y git wget python3 python3-pip python3-dev libffi-dev libssl-dev cargo sed build-essential apache2 php libapache2-mod-php php-sqlite3 > /dev/null
+  # Check if the server is already installed
+  if [ -d /opt/adnserver ]; then
+    # read -p "ADN Systems DMR Server is already installed! Do you want to update it from the latest version? (y/n): " update_choice
+    # if [[ "$update_choice" != "y" && "$update_choice" != "Y" ]]; then
+    #   echo "Exiting without making any changes."
+    #   exit 0
+    # fi
+    echo "Stopping services before updating..."
+    systemctl stop adn_server.service > /dev/null
+    systemctl stop adn_parrot.service > /dev/null
+    echo "Updating ADN Systems DMR Server..."
+  else
+    # Update and install required packages
+    echo "Updating package list and installing required packages... (this can take a while!)"
+    apt-get update > /dev/null
+    apt-get install -y git wget python3 python3-pip python3-dev libffi-dev libssl-dev cargo sed build-essential apache2 php libapache2-mod-php php-sqlite3
+    # Install Python packages
+    pip3 install --no-cache-dir bitstring bitarray Twisted dmr_utils3 configparser resettabletimer setproctitle Pyro5 spyne setuptools wheel autobahn jinja2 MarkupSafe pyOpenSSL service-identity bitarray
+    show_adnsystems
+    echo "Installing ADN Systems DMR Server..."
+  fi
 
-  # Install Python packages
-  echo "Installing Python packages..."
-  pip3 install --no-cache-dir setuptools wheel Twisted dmr_utils3 bitstring autobahn jinja2 MarkupSafe pyOpenSSL service-identity bitarray configparser resettabletimer setproctitle Pyro5 spyne > /dev/null
-fi
+  # Clone or update the server repository
+  if [ -d /opt/adnserver ]; then
+    cd /opt/adnserver
+    git pull --ff-only > /dev/null
+  else
+    cd /opt
+    git clone https://github.com/Amateur-Digital-Network/ADN-DMR-Peer-Server.git adnserver > /dev/null
+  fi
 
-# Clone or update the dashboard repository
-if [ -d /opt/dashboard ]; then
+  # Create log directory if not exists
+  mkdir -p /opt/adnserver/log
+
+  # Check and copy configuration files if not present
+  cd /opt/adnserver/config
+  if [ ! -f adn.cfg ]; then
+    cp ADN-SAMPLE.cfg adn.cfg
+    sed -i 's|LOG_FILE: /dev/null|LOG_FILE: /opt/adnserver/log/adnserver.log|' adn.cfg
+    sed -i 's|LOG_HANDLERS: console-timed|LOG_HANDLERS: file-timed|' adn.cfg
+    sed -i 's|LOG_LEVEL: DEBUG|LOG_LEVEL: INFO|' adn.cfg
+  fi
+
+  # Create log rotation configuration
+  if [ ! -f /etc/logrotate.d/adnserver ]; then
+    echo "Creating ADN Systems DMR Server log rotation configuration..."
+    cat <<EOL > /etc/logrotate.d/adnserver
+/opt/adnserver/log/adnserver.log {
+    daily
+    rotate 7
+    size 10M
+    compress
+    delaycompress
+    copytruncate
+}
+EOL
+  fi
+
+  # Create adn_server service
+  if [ ! -f /etc/systemd/system/adn_server.service ]; then
+    echo "Creating ADN Systems DMR Server service (adn_server.service)..."
+    cat <<EOL > /etc/systemd/system/adn_server.service
+[Unit]
+Description=ADN Systems DMR Server
+After=network-online.target syslog.target
+
+[Service]
+User=root
+WorkingDirectory=/opt/adnserver
+ExecStart=/usr/bin/python3 /opt/adnserver/bridge_master.py -c /opt/adnserver/config/adn.cfg
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOL
+  fi
+
+  # Create adn_parrot service
+  if [ ! -f /etc/systemd/system/adn_parrot.service ]; then
+    echo "Creating ADN Systems DMR Server Parrot service (adn_parrot.service)..."
+    cat <<EOL > /etc/systemd/system/adn_parrot.service
+[Unit]
+Description=ADN Systems Parrot
+After=network-online.target syslog.target
+
+[Service]
+User=root
+WorkingDirectory=/opt/adnserver
+ExecStart=/usr/bin/python3 /opt/adnserver/playback.py -c /opt/adnserver/config/parrot.cfg
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOL
+  fi
+
+  # Enable services
+  echo "Enabling services..."
+  systemctl enable adn_server.service > /dev/null
+  systemctl enable adn_parrot.service > /dev/null
+
+  # Start services
+  echo "Starting services..."
+  systemctl start adn_server.service > /dev/null
+  systemctl start adn_parrot.service > /dev/null
+
+  echo "ADN Systems DMR Server installation/update complete."
+}
+
+# Function to install/update the ADN Dashboard
+install_update_dashboard() {
+  # Check if TCP port 9000 is in use and /opt/dashboard does not exist
+  if netstat -tuln | grep ':9000' > /dev/null && [ ! -d /opt/dashboard ]; then
+    echo "It seems that you have a custom Dashboard installation already installed. Please remove the current installation and run this script again."
+    exit 1
+  fi
+
+  # Check if the dashboard is already installed
+  if [ -d /opt/dashboard ]; then
+    # read -p "Dashboard is already installed. Do you want to update it from the latest version on GitHub? (y/n): " update_choice
+    # if [[ "$update_choice" != "y" && "$update_choice" != "Y" ]]; then
+    #   echo "Exiting without making any changes."
+    #   exit 0
+    # fi
+    echo "Stopping services before updating..."
+    systemctl stop adn_proxy.service > /dev/null
+    systemctl stop adn_dashboard.service > /dev/null
+    echo "Updating the dashboard..."
+  else
+    echo "Installing the ADN Systems Dashboard..."
+
+    # Update and install required packages
+    #echo "Updating package list and installing required packages... (this can take a while)"
+    #apt-get update > /dev/null
+    #apt-get install -y git wget python3 python3-pip python3-dev libffi-dev libssl-dev cargo sed build-essential apache2 php libapache2-mod-php php-sqlite3
+
+    # Install Python packages
+    #pip3 install --no-cache-dir bitstring bitarray Twisted dmr_utils3 configparser resettabletimer setproctitle Pyro5 spyne setuptools wheel autobahn jinja2 MarkupSafe pyOpenSSL service-identity 
+  fi
+
+  # Clone or update the dashboard repository
+  if [ -d /opt/dashboard ]; then
+    cd /opt/dashboard
+    git pull --ff-only > /dev/null
+  else
+    cd /opt
+    git clone https://github.com/Amateur-Digital-Network/ADN-Dashboard.git dashboard > /dev/null
+  fi
+
+  # Check and copy configuration files if not present
   cd /opt/dashboard
-  git pull --ff-only > /dev/null
-else
-  cd /opt
-  git clone https://github.com/Amateur-Digital-Network/ADN-Dashboard.git dashboard > /dev/null
-fi
+  if [ ! -f dashboard.cfg ]; then
+    cp dashboard_SAMPLE.cfg dashboard.cfg
+  fi
 
-# Check and copy configuration files if not present
-cd /opt/dashboard
-if [ ! -f dashboard.cfg ]; then
-  cp dashboard_SAMPLE.cfg dashboard.cfg
-fi
+  cd /opt/dashboard/proxy
+  if [ ! -f proxy.cfg ]; then
+    cp proxy_SAMPLE.cfg proxy.cfg
+  fi
 
-cd /opt/dashboard/proxy
-if [ ! -f proxy.cfg ]; then
-  cp proxy_SAMPLE.cfg proxy.cfg
-fi
+  cd /opt/dashboard
 
-cd /opt/dashboard
+  # Create the database file only if installing
+  if [ ! -f /opt/dashboard/html/db/dashboard.db ]; then
+    echo "Creating the database file..."
+    python3 dash_db.py > /dev/null
+  fi
 
-# Create the database file only if installing
-if [ ! -f /opt/dashboard/html/db/dashboard.db ]; then
-  echo "Creating the database file..."
-  python3 dash_db.py > /dev/null
-fi
+  # Set ownership of the html directory
+  echo "Setting ownership of the dashboard/html directory..."
+  chown -R www-data:www-data /opt/dashboard/html/
 
-# Set ownership of the html directory
-echo "Setting ownership of the html directory..."
-chown -R www-data:www-data /opt/dashboard/html/
-
-# Create Apache configuration
-echo "Creating Apache configuration..."
-cat <<EOL > /etc/apache2/sites-available/adndash-default.conf
+  # Create Apache configuration
+  echo "Creating Apache configuration..."
+  cat <<EOL > /etc/apache2/sites-available/adndash-default.conf
 <VirtualHost *:80>
 	ServerAdmin webmaster@localhost
 	DocumentRoot /opt/dashboard/html
@@ -111,14 +240,14 @@ cat <<EOL > /etc/apache2/sites-available/adndash-default.conf
 </VirtualHost>
 EOL
 
-# Disable default Apache site and enable the new site
-echo "Configuring Apache..."
-a2dissite 000-default.conf > /dev/null
-a2ensite adndash-default.conf > /dev/null
+  # Disable default Apache site and enable the new site
+  echo "Configuring Apache..."
+  a2dissite 000-default.conf > /dev/null
+  a2ensite adndash-default.conf > /dev/null
 
-# Update Apache configuration
-if ! grep -q "<Directory /opt/dashboard/>" /etc/apache2/apache2.conf; then
-  cat <<EOL >> /etc/apache2/apache2.conf
+  # Update Apache configuration
+  if ! grep -q "<Directory /opt/dashboard/>" /etc/apache2/apache2.conf; then
+    cat <<EOL >> /etc/apache2/apache2.conf
 
 <Directory /opt/dashboard/>
 	Options Indexes FollowSymLinks
@@ -126,12 +255,12 @@ if ! grep -q "<Directory /opt/dashboard/>" /etc/apache2/apache2.conf; then
 	Require all granted
 </Directory>
 EOL
-fi
+  fi
 
-# Create log rotation configuration
-if [ ! -f /etc/logrotate.d/dashboard ]; then
-  echo "Creating log rotation configuration..."
-  cat <<EOL > /etc/logrotate.d/dashboard
+  # Create log rotation configuration
+  if [ ! -f /etc/logrotate.d/dashboard ]; then
+    echo "Creating ADN Syatems Dashboard log rotation configuration..."
+    cat <<EOL > /etc/logrotate.d/dashboard
 /opt/dashboard/log/dashboard.log {
     daily
     rotate 7
@@ -141,12 +270,12 @@ if [ ! -f /etc/logrotate.d/dashboard ]; then
     copytruncate
 }
 EOL
-fi
+  fi
 
-# Create adn_dashboard service
-if [ ! -f /etc/systemd/system/adn_dashboard.service ]; then
-  echo "Creating ADN Dashboard service..."
-  cat <<EOL > /etc/systemd/system/adn_dashboard.service
+  # Create adn_dashboard service
+  if [ ! -f /etc/systemd/system/adn_dashboard.service ]; then
+    echo "Creating ADN Systems Dashboard service (adn_dashboard.service)..."
+    cat <<EOL > /etc/systemd/system/adn_dashboard.service
 [Unit]
 Description=ADN Dashboard
 After=network-online.target syslog.target
@@ -162,12 +291,12 @@ Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 EOL
-fi
+  fi
 
-# Create adn_proxy service
-if [ ! -f /etc/systemd/system/adn_proxy.service ]; then
-  echo "Creating Proxy service..."
-  cat <<EOL > /etc/systemd/system/adn_proxy.service
+  # Create adn_proxy service
+  if [ ! -f /etc/systemd/system/adn_proxy.service ]; then
+    echo "Creating ADN Systems Proxy service (adn_proxy.service)..."
+    cat <<EOL > /etc/systemd/system/adn_proxy.service
 [Unit]
 Description=ADN Server Proxy
 After=multi-user.target
@@ -182,18 +311,66 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOL
+  fi
+
+  # Enable services
+  echo "Enabling services..."
+  systemctl enable apache2 > /dev/null
+  systemctl enable adn_proxy.service > /dev/null
+  systemctl enable adn_dashboard.service > /dev/null
+
+  # Start services
+  echo "Starting services..."
+  systemctl restart apache2 > /dev/null
+  systemctl start adn_proxy.service > /dev/null
+  systemctl start adn_dashboard.service > /dev/null
+
+  echo "ADN Systems Dashboard installation/update complete."
+}
+
+# Main script execution
+dashboard_installed=false
+server_installed=false
+
+# Check if Dashboard or Server is already installed
+if [ -d /opt/dashboard ]; then
+  dashboard_installed=true
+fi
+if [ -d /opt/adnserver ]; then
+  server_installed=true
 fi
 
-# Enable services
-echo "Enabling services..."
-systemctl enable apache2 > /dev/null
-systemctl enable adn_proxy.service > /dev/null
-systemctl enable adn_dashboard.service > /dev/null
+# Prompt user for action
+if $dashboard_installed || $server_installed; then
+  echo "ADN Systems Server and Dashboard are already installed."
+  echo "Do you want to update:"
+  echo "1 - only the DMR Server"
+  echo "2 - only the Dashboard"
+  echo "3 - both DMR Server and Dashboard"
+  echo "4 - Exit"
+  read -p "Choose (1/2/3/4): " update_choice
+  case $update_choice in
+    1) show_adnsystems; install_update_server ;;
+    2) show_adnsystems; install_update_dashboard ;;
+    3) show_adnsystems; install_update_dashboard; install_update_server ;;
+    4) echo "Exiting without making any changes."; exit 0 ;;
+    *) echo "Invalid choice."; exit 1 ;;
+  esac
+else
+  read -p "Do you want to install ADN Systems DMR Server with Dashboard? (y/n): " install_choice
+  if [[ "$install_choice" != "y" && "$install_choice" != "Y" ]]; then
+    echo "Exiting without making any changes."
+    exit 0
+  fi
+  show_adnsystems
+  install_update_server
+  install_update_dashboard
+fi
 
-# Start services
-echo "Starting services..."
-systemctl restart apache2 > /dev/null
-systemctl start adn_proxy.service > /dev/null
-systemctl start adn_dashboard.service > /dev/null
+# Checking services status
+systemctl status adn_server.service
+systemctl status adn_proxy.service
+systemctl status adn_parrot.service
+systemctl status adn_dashboard.service
 
-echo "ADN Dashboard and Proxy installation complete."
+echo "You're done. Enjoy!"
