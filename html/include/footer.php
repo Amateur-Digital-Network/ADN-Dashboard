e7ab14f1d88623f43a9c572752336079f6648192
Bruno <91850710+CS8ABG@users.noreply.github.com>
2024-05-31 20:44:07 +0000

    version

--- html/include/footer.php
+++ html/include/footer.php
@@ -1,20 +1,21 @@
-<!-- Credits: SP2ONG 2019-2022 -->
-<!-- Credits: OA4DOA 2022 -->
+<!-- Credits: Cortney T. Buffington, N0MJS 2013-2018 | https://github.com/tolitski/HBmonitor.git -->
+<!-- Credits: KC1AWV 2019 | https://github.com/kc1awv/hbmonitor3.git-->
+<!-- Credits: SP2ONG 2019-2022 | https://github.com/sp2ong/HBmonitor.git -->
+<!-- Credits: OA4DOA 2022 | https://github.com/yuvelq/FDMR-Monitor.git -->
 <!-- THIS COPYRIGHT NOTICE MUST BE DISPLAYED AS A CONDITION OF THE LICENCE GRANT FOR THIS SOFTWARE. ALL DERIVATEIVES WORKS MUST CARRY THIS NOTICE -->
 <div class="text-center d-none d-sm-inline">
-        <div><?php
+    <div>
+        <?php
         if (isset($config['DASHBOARD']['FOOTER1']) && !empty($config['DASHBOARD']['FOOTER1'])) {
             $footer1Value = $config['DASHBOARD']['FOOTER1'];
             echo $footer1Value . ' | ';
         }
-        ?>The Regents of the K0USY Group. | <a
-                        title="FDMR Monitor OA4DOA v270422" target="_blank"
-                        href="https://github.com/yuvelq/FDMR-Monitor.git">OA4DOA</a> Version 2022 | <a title="CS8ABG Dash v24.05" href=https://github.com/CS8ABG/FDMR-Monitor.git>CS8ABG</a> ADN Systems Dashboard
+        ?><a title="CS8ABG Dash v24.06" href=https://github.com/Amateur-Digital-Network/ADN-Dashboard.git>CS8ABG</a> ADN Systems Dashboard
                         <?php
         if (isset($config['DASHBOARD']['FOOTER2']) && !empty($config['DASHBOARD']['FOOTER2'])) {
             $footer2Value = $config['DASHBOARD']['FOOTER2'];
             echo ' | ' . $footer2Value;
         }
         ?>
-                </div>
+    </div>
 </div>
