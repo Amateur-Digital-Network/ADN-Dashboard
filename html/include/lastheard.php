50285f502809a77fd03fa9d68966391a7d2442f5
hp3icc <setcom40@gmail.com>
2025-10-10 11:34:01 -0500

    .

--- html/include/lastheard.php
+++ html/include/lastheard.php
@@ -1,19 +1,31 @@
+
 <div class="container">
+
+    <!-- iz6rnd -->
+    <div id="log" style="display: none;"></div>
+    
     <p id="lsthrd_log"></p>
-  </div>
+    
+</div>
+
 <!-- this checks if id="lsthrd_log" as content -->
+
 <script>
   window.addEventListener('load', function () {
+    var log_box = document.getElementById('log');
+    // if (log_box) log_box.innerHTML = ""; // iz6rnd
     setInterval(checkMainContent, 5000);
   });
 
   function checkMainContent() {
     var mainElement = document.getElementById('lsthrd_log');
     if (mainElement && mainElement.innerHTML.trim() !== '') {
+    // if (mainElement) { -- iz6rnd
       // console.log('OKAY');
     } else {
-      //console.log('NotOK');
+      // console.log('NotOK');
       location.reload();
     }
   }
 </script>
+
