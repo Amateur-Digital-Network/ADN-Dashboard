ad8bcb4e1f22e1524415b38db5958de5c17d1ad1
Esteban Mackay Q. <49044505+hp3icc@users.noreply.github.com>
2026-01-29 11:39:33 -0500

    Add files via upload

---
+++ html/include/self.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header border-transparent">
+          <h3 class="card-title">Selfcare ADN</h3>
+        </div>
+        <div class="card-body p-0">
+          <iframe
+            src="https://selfcare.adn.systems"
+            style="width:100%; height:100vh; border:0;"
+            loading="lazy"
+            referrerpolicy="no-referrer"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
